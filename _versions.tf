14308aa6bb598c91ec35668c8def1eaea4c90069
Christopher Basketfield <chris.basketfield@gmail.com>
2022-10-11 14:15:29 -0800

    versions

--- _versions.tf
+++ _versions.tf
@@ -1,13 +1,11 @@
 terraform {
+  required_version = "~>1.3"
   cloud {
     organization = "my-org"
     workspaces {
       name = "my-workspace"
     }
   }
-  # experiments = [
-  #   module_variable_optional_attrs
-  # ]
   required_providers {
   }
 }
